0cfa27fb923625eac5d9d92251142888a7c03de1
kukuh2307 <kukuhagung12@gmail.com>
2023-07-31 09:19:44 +0700

    menampilkan semua data dari file

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ArticleController extends Controller
 {
     public function index(){
+        $storage = Storage::get('articles.txt');
+        echo $storage;
+        exit;
         $variable = [
             'articles' => [
                 ['mengenal laravel','ini adalah contoh array static'],
