a8d0c852a75481e7ab553168e97d15e83ca74b9e
kukuh2307 <kukuhagung12@gmail.com>
2023-08-20 23:00:18 +0700

    (modify) menambahkan header dan footer pada index.blade.php

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -18,6 +18,25 @@
     </style>
 </head>
 <body>
+    {{-- header --}}
+    <header class="p-3 text-bg-dark mb-3">
+        <div class="container">
+          <div class="d-flex flex-wrap align-items-center justify-content-between">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+              <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+            </a>
+    
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-between mb-md-0">
+              <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+    
+            <div class="text-end">
+              <button type="button" class="btn btn-outline-light me-2">Login</button>
+            </div>
+          </div>
+        </div>
+      </header>
+      {{-- akhir header --}}
+
     <div class="container">
         <h1>Ini adalah content dari article
             <a href="{{ url("article/create") }}"><button class="btn btn-primary">Tambah Artikel</button></a> 
@@ -34,6 +53,20 @@
             </div>
         </div>
         @endforeach
-    </div>
+
+    {{-- footer --}}
+    <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          <p class="col-md-4 mb-0 text-body-secondary">© 2023 Company, Inc</p>
+      
+          <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+          </a>
+      
+          <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+          </ul>
+        </footer>
+      </div>
 </body>
 </html>
